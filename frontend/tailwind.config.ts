9de6d4cacf6193e09cd97c44f869456ae72eaa59
fhcabral <fhellipeasd123@gmail.com>
2024-12-21 04:12:35 -0400

    Creating new config setup for application

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./pages/**/*.vue",
+    "./app.vue",
+    "./error.vue",
+    "./layouts/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        darkTheme: {
+          100: "#121212",
+          200: "#20212C",
+        },
+        lightMode: {
+          100: "#FFFFFF",
+          200: "#F9F9F9",
+        },
+        custom: {
+          50: '#F5F7F4',   // Mais claro
+          100: '#4B5945',  // Mais escuro
+          200: '#66785F',
+          300: '#91AC8F',
+          400: '#B2C9AD'   // Mais claro
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
